677033eff4b57c59b88099df76ac7e5540fd84ac
Jimmy Tang <jtang@tchpc.tcd.ie>
2010-09-14 12:03:11 +0100

    fix up bootstrap and autoconf dependancy in macros

--- mk/gnu.post.mk
+++ mk/gnu.post.mk
@@ -1034,7 +1034,7 @@ endif
 # Use autoconf to rebuild configure-script
 #
 do-autoreconf:
-	$(QUIET) $(MODULE_ADD) $(BUILD_DEPENDS) autoconf/2.66$(COMPILER_TAG); \
+	$(QUIET) $(MODULE_ADD) $(BUILD_DEPENDS) autoconf/2.67$(COMPILER_TAG); \
 	cd $(WRKSRC) && autoreconf
 
 #
