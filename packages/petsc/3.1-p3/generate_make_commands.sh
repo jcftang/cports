edf23474ab34564871dabb8132ed24b2959e6fc0
Jimmy Tang <jtang@cports-devel.tchpc.tcd.ie>
2010-09-18 22:19:42 +0100

    reorder the way the shell script outputs commands

--- packages/petsc/3.1-p3/generate_make_commands.sh
+++ packages/petsc/3.1-p3/generate_make_commands.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for l in build install clean;
+for i in debug nodebug;
 do
     for k in c cxx;
     do
 	for j in real complex;
 	do
-	    for i in debug nodebug;
+	    for l in build install clean;
 	    do
 		echo make CPRT_PETSC_DEBUG=$i CPRT_PETSC_SCALAR=$j CPRT_PETSC_CCOMP=$k $l $@
 	    done
