90c9feeeff2916c10868e1f5fdd6e080606ad971
Darach Golden <darach@tchpc.tcd.ie>
2010-09-20 11:17:03 +0100

    in no particular order:
    removed petsc; created petsc-basic, petsc-directsolve -- i.e., various hopefully useful compiles
    of petsc, with petsc-basic being pretty much out of the box
    
    split mumps into compiles against various ordering tools: scotch, parmetis, pord (the default)
    this happened because petsc claims scotch doesn't support complex numbers, so need to use
    parmetis for complex number petsc compiles
    
    minor changes to superlu dist file

---
+++ packages/petsc-basic/3.1-p3/generate_make_commands.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for l in build install clean;
+do
+    for k in c cxx;
+    do
+	for j in real complex;
+	do
+	    for i in debug nodebug;
+	    do
+		echo make CPRT_PETSC_DEBUG=$i CPRT_PETSC_SCALAR=$j CPRT_PETSC_CCOMP=$k $l $@
+	    done
+	done
+    done
+done
