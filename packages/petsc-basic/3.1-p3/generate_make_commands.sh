4a29ab46f6859615311bd9e085acfe1910de2457
Darach Golden <darach@tchpc.tcd.ie>
2010-10-17 23:53:22 +0100

    added CPRT_PETSC_BLAS...  to  makefile command generator

--- packages/petsc-basic/3.1-p3/generate_make_commands.sh
+++ packages/petsc-basic/3.1-p3/generate_make_commands.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+for a in atlas mkl
+do
 for l in build install clean;
 do
     for k in c cxx;
@@ -8,8 +10,9 @@ do
 	do
 	    for i in debug nodebug;
 	    do
-		echo make CPRT_PETSC_DEBUG=$i CPRT_PETSC_SCALAR=$j CPRT_PETSC_CCOMP=$k $l $@
+		echo make CPRT_PETSC_DEBUG=$i CPRT_PETSC_SCALAR=$j CPRT_PETSC_CCOMP=$k CPRT_PETSC_BLAS=$a $l $@
 	    done
 	done
     done
+done
 done
