ff7528f5750080b0e5ff9f601d9bd0c1de04982c
Jimmy Tang <jtang@cports-devel.tchpc.tcd.ie>
2010-11-11 12:00:59 +0000

    remove old files

--- t/tgnu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. ../scripts/wvtest.sh
-
-# assume default gcc4 (with gfortran and not g77)
-WVSTART "GNU compilers"
-WVPASS gcc --version
-WVPASS g++ --version
-WVPASS gfortran --version
--- t/tintel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. ../scripts/wvtest.sh
-
-WVSTART "Intel compilers"
-WVPASS icc --version
-WVPASS icpc --version
-WVPASS ifort --version
