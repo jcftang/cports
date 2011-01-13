ee64ac60910d50da86f84fddb6455a9c59070376
Jimmy Tang <jtang@cports-devel.tchpc.tcd.ie>
2011-01-13 15:50:22 +0000

    update bootstrap test, don't do 2.0 tests since it requires some things to be installed.

--- t/t3010-gnutbootstrap.sh
+++ t/t3010-gnutbootstrap.sh
@@ -15,7 +15,7 @@ WVPASSRC $?
 WVSTART "bootstrap/1.0"
 (cd ../packages/ncurses/5.7/ && make build)
 WVPASSRC $?
-(cd ../packages/m4/1.4.14/ && make BOOTSTRAP=yes build)
+(cd ../packages/m4/1.4.15/ && make BOOTSTRAP=yes build)
 WVPASSRC $?
 (cd ../packages/texinfo/4.13/ && make BOOTSTRAP=yes build)
 WVPASSRC $?
@@ -29,27 +29,27 @@ WVPASSRC $?
 WVPASSRC $?
 (cd ../packages/libtool/2.2.10/ && make BOOTSTRAP=yes build)
 
-# clean
-WVSTART "Clean before bootstrap/2.0"
-(cd ../packages/bootstrap/1.0; make do-cleaz)
-WVPASSRC $?
-(cd ../packages/bootstrap/2.0; make do-cleaz)
-WVPASSRC $?
-
-#bootstrap 2.0
-WVSTART "bootstrap/2.0"
-(cd ../packages/texinfo/4.13/ && make build)
-WVPASSRC $?
-(cd ../packages/gmake/3.81/ && make build)
-WVPASSRC $?
-(cd ../packages/wget/1.9.1/ && make build)
-WVPASSRC $?
-(cd ../packages/libiconv/1.13/ && make build)
-WVPASSRC $?
-(cd ../packages/gettext/0.18.1.1/ && make build)
-WVPASSRC $?
-(cd ../packages/libtool/2.2.10/ && make build)
-WVPASSRC $?
+## # clean
+## WVSTART "Clean before bootstrap/2.0"
+## (cd ../packages/bootstrap/1.0; make do-cleaz)
+## WVPASSRC $?
+## (cd ../packages/bootstrap/2.0; make do-cleaz)
+## WVPASSRC $?
+## 
+## #bootstrap 2.0
+## WVSTART "bootstrap/2.0"
+## (cd ../packages/texinfo/4.13/ && make build)
+## WVPASSRC $?
+## (cd ../packages/gmake/3.81/ && make build)
+## WVPASSRC $?
+## (cd ../packages/wget/1.9.1/ && make build)
+## WVPASSRC $?
+## (cd ../packages/libiconv/1.13/ && make build)
+## WVPASSRC $?
+## (cd ../packages/gettext/0.18.1.1/ && make build)
+## WVPASSRC $?
+## (cd ../packages/libtool/2.2.10/ && make build)
+## WVPASSRC $?
 
 # clean
 WVSTART "Clean up"
