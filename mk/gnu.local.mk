85772e3d3eda8a8de66fc226853880b1409fb0a4
Darach Golden <darach@tchpc.tcd.ie>
2011-01-15 01:53:33 +0000

    gnu.local.mk edited to be more general

--- mk/gnu.local.mk
+++ mk/gnu.local.mk
@@ -3,7 +3,17 @@
 #
 # Define Compiler suite profiles
 #
-ifeq (gnu4.5.2,$(COMPILERS))
+
+#$(info COMPILERS is $(COMPILERS))
+
+CPRT_COMPILER_CLASS:=nocompiler
+CPRT_COMPILER_CLASS:=$(strip $(if $(findstring intel, $(COMPILERS)),intel,$(CPRT_COMPILER_CLASS)))
+CPRT_COMPILER_CLASS:=$(strip $(if $(findstring gnu, $(COMPILERS)),gnu,$(CPRT_COMPILER_CLASS)))
+CPRT_COMPILER_CLASS:=$(strip $(if $(findstring open, $(COMPILERS)),open64,$(CPRT_COMPILER_CLASS)))
+
+#$(info CPRT_COMPILER_CLASS is $(CPRT_COMPILER_CLASS))
+
+ifeq (gnu,$(CPRT_COMPILER_CLASS))
 ENVIRONMENT+= CC=gcc
 ENVIRONMENT+= CXX=g++
 ENVIRONMENT+= F77=gfortran
@@ -11,66 +21,12 @@ ENVIRONMENT+= F90=gfortran
 ENVIRONMENT+= FC=gfortran
 COMPILER_VERSION?= $(shell gcc -dumpversion)
 COMPILER_TAG?=-gnu${COMPILER_VERSION}
-ifneq (-gnu4.5.2,$(COMPILER_TAG))
+ifneq ($(COMPILER_TAG),-$(COMPILERS))
 COMPILER_TAG= echo "Error compiler that is loaded up is wrong"
 endif
 endif
 
-
-ifeq (gnu4.5.1,$(COMPILERS))
-ENVIRONMENT+= CC=gcc
-ENVIRONMENT+= CXX=g++
-ENVIRONMENT+= F77=gfortran
-ENVIRONMENT+= F90=gfortran
-ENVIRONMENT+= FC=gfortran
-COMPILER_VERSION?= $(shell gcc -dumpversion)
-COMPILER_TAG?=-gnu${COMPILER_VERSION}
-ifneq (-gnu4.5.1,$(COMPILER_TAG))
-COMPILER_TAG= echo "Error compiler that is loaded up is wrong"
-endif
-endif
-
-
-ifeq (gnu4.5.0,$(COMPILERS))
-ENVIRONMENT+= CC=gcc
-ENVIRONMENT+= CXX=g++
-ENVIRONMENT+= F77=gfortran
-ENVIRONMENT+= F90=gfortran
-ENVIRONMENT+= FC=gfortran
-COMPILER_VERSION?= $(shell gcc -dumpversion)
-COMPILER_TAG?=-gnu${COMPILER_VERSION}
-ifneq (-gnu4.5.0,$(COMPILER_TAG))
-COMPILER_TAG= echo "Error compiler that is loaded up is wrong"
-endif
-endif
-
-ifeq (gnu4.4.0,$(COMPILERS))
-ENVIRONMENT+= CC=gcc
-ENVIRONMENT+= CXX=g++
-ENVIRONMENT+= F77=gfortran
-ENVIRONMENT+= F90=gfortran
-ENVIRONMENT+= FC=gfortran
-COMPILER_VERSION?= $(shell gcc -dumpversion)
-COMPILER_TAG?=-gnu${COMPILER_VERSION}
-ifneq (-gnu4.4.0,$(COMPILER_TAG))
-COMPILER_TAG= echo "Error compiler that is loaded up is wrong"
-endif
-endif
-
-ifeq (gnu4.4.4,$(COMPILERS))
-ENVIRONMENT+= CC=gcc
-ENVIRONMENT+= CXX=g++
-ENVIRONMENT+= F77=gfortran
-ENVIRONMENT+= F90=gfortran
-ENVIRONMENT+= FC=gfortran
-COMPILER_VERSION?= $(shell gcc -dumpversion)
-COMPILER_TAG?=-gnu${COMPILER_VERSION}
-ifneq (-gnu4.4.4,$(COMPILER_TAG))
-COMPILER_TAG= echo "Error compiler that is loaded up is wrong"
-endif
-endif
-
-ifeq (open644.2.3,$(COMPILERS))
+ifeq (open64,$(CPRT_COMPILER_CLASS))
 ENVIRONMENT+= CC=opencc
 ENVIRONMENT+= CXX=openCC
 ENVIRONMENT+= F77=openf90
@@ -78,12 +34,12 @@ ENVIRONMENT+= F90=openf90
 ENVIRONMENT+= FC=openf95
 COMPILER_VERSION?= $(shell opencc -dumpversion)
 COMPILER_TAG?=-open64${COMPILER_VERSION}
-ifneq (-open644.2.3,$(COMPILER_TAG))
+ifneq ($(COMPILER_TAG),-$(COMPILERS))
 COMPILER_TAG= echo "Error compiler that is loaded up is wrong"
 endif
 endif
 
-ifeq (intel11.0,$(COMPILERS))
+ifeq (intel,$(CPRT_COMPILER_CLASS))
 ENVIRONMENT+= CC=icc
 ENVIRONMENT+= CXX=icpc
 ENVIRONMENT+= F77=ifort
@@ -91,20 +47,12 @@ ENVIRONMENT+= F90=ifort
 ENVIRONMENT+= FC=ifort
 COMPILER_VERSION?= $(shell icc -dumpversion)
 COMPILER_TAG?=-intel${COMPILER_VERSION}
-ifneq (-intel11.0,$(COMPILER_TAG))
+ifneq ($(COMPILER_TAG),-$(COMPILERS))
 COMPILER_TAG= echo "Error compiler that is loaded up is wrong"
 endif
 endif
 
-ifeq (intel10.1,$(COMPILERS))
-ENVIRONMENT+= CC=icc
-ENVIRONMENT+= CXX=icpc
-ENVIRONMENT+= F77=ifort
-ENVIRONMENT+= F90=ifort
-ENVIRONMENT+= FC=ifort
-COMPILER_VERSION?= $(shell icc -dumpversion)
-COMPILER_TAG?=-intel${COMPILER_VERSION}
-ifneq (-intel10.1,$(COMPILER_TAG))
-COMPILER_TAG= echo "Error compiler that is loaded up is wrong"
-endif
-endif
+#$(info ENVIRONMENT: $(ENVIRONMENT))
+#$(info COMPILER VERSION  is $(COMPILER_VERSION))
+#$(info COMPILER TAG is $(COMPILER_TAG))
+
