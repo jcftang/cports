cb42350c55d06936c983bbf392189f211714a108
Jimmy Tang <jtang@tchpc.tcd.ie>
2011-06-29 16:29:12 +0100

    Autogenerated html pages for 0.8.1.0-33-gaef145c

--- packages/fftw/3.2.2/generate_make_commands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for k in double long-double float;
-do
-  for l in build install clean;
-    do
-    echo make FFTW_PRECISION=$k $l $@
-  done
-done
--- packages/gt/5.0.2/README.txt
+++
@@ -1,2 +0,0 @@
-This Globus install is still just an install process, it is still up
-to the administrator/user to configure Globus before it is useful.
