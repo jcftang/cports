82e3defd6d68844c648a7b8ed8903647015b31ad
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-07-18 14:23:23 +0100

    added some useful macros for adding and removing lines

--- deploy/puppet/manifests/functions.pp
+++ deploy/puppet/manifests/functions.pp
@@ -19,3 +19,18 @@ define line($file, $line, $ensure = 'present') {
         }
     }
 }
+
+define append_if_no_such_line($file, $line, $refreshonly = 'false') {
+   exec { "/bin/echo '$line' >> '$file'":
+      unless      => "/bin/grep -Fxqe '$line' '$file'",
+      path        => "/bin",
+      refreshonly => $refreshonly,
+   }
+}
+
+define delete_lines($file, $pattern) {
+   exec { "sed -i -r -e '/$pattern/d' $file":
+      path   => "/bin",
+      onlyif => "/bin/grep -E '$pattern' '$file'",
+   }
+}
